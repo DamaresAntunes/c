a39e77d3068e079796411882758ec5c679f64dbf
DamaresAntunes <117866167+DamaresAntunes@users.noreply.github.com>
2023-08-09 10:15:13 +0100

    09/08
    09/08

---
+++ envio.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+require("php/src/PHPMailer.php");
+require("php/src/SMTP.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465; // 465 or 587 NO SSL
+$mail->IsHTML(true);
+$mail->Username = "damaresdejesusantunes@gmail.com"; // servidor de envio
+$mail->Password = "xeuhsbxmctetgiac";
+$mail->SetFrom($_POST["email"]);
+$mail->Subject = "No Subject";
+$mail->Body = $_POST["name"]."<br>".$_POST["email"]."<br>".$_POST["contato"]."<br><br>".$_POST["textarea"] ;
+$mail->AddAddress("damaresdejesusantunes@gmail.com"); // quem vai receber o email
+
+if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+    return false;
+} else {
+    echo "Mensagem enviada com sucesso";
+    return true;
+}
+?>
+
+</body>
+</html>
